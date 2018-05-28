d2a3b43977c291c911bc9a4337d5223d901c9a21
alexandra-b <alexandra.bocereg@gmail.com>
2018-05-28 22:08:28 +0300

    Arduino To Pi Commuication Fix

--- codebase/sendDataFromArduino.c
+++ codebase/sendDataFromArduino.c
@@ -3,28 +3,35 @@
 
 //Digital Pins: CE, CSN
 RF24 radio(9, 10);
-int PIR_output = 5; // Output of Passive Infra-Red
 
-void setup(){
+
+int sensor = 2;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+
+void setup() {
+/*
   radio.begin()
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(0x76);
   radio.openWritingPipe(0xF0F0F0E1LL); //need to match the one from R-Pi
   radio.enableDynamicPayloads();
   radio.powerUp();
-
-  pinMode(PIR_output, INPUT);
-  Serial.begin(9600);
+*/
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  digitalWrite(2, 1);
 }
 
 void loop(){
-  const char text[] = "Hello World";
-  if(digitalRead(PIR_output) == HIGH){
-    radio.write(&text, sizeof(text));
-    delay(1000);
-    Serial.println("Motion detected!");
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {
+    // check if the sensor is HIGH
+    if(state == LOW){
+      Serial.begin(9600);        // initialize serial
+      //radio.write(&text, sizeof(text));
+      Serial.println("1");
+      Serial.end();
+    }
   }
-  else{
-    Serial.println("Scanning...");
-  }
+  state=val;
 }
